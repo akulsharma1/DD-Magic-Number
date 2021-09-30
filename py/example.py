0cf3fcee4f52ef2b9a48218a0d44fb77d953e0b0
AKUL SHARMA <akulsharma02@gmail.com>
2021-09-30 09:30:55 -0700

    removed python

--- py/example.py
+++
@@ -1,9 +0,0 @@
-from magic_number import DatadomeMagicNumber
-
-print(
-    DatadomeMagicNumber(
-        ".AmpSWXKxiOOE-dZx0XOos9WBjFdz5sINnD4ma-tBP2mzH3wZlSTjDWQ-vS9-W2Vif7SLH8o~CKpsVhkWh2apXl03PnDfJl7i0N_nBvDiZg_9JhGz0biH6qc~uU8gu.g",
-        10,
-        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.85 Safari/537.36",
-    ).Generate()
-) #181129178
